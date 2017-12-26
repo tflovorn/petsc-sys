0035400b87b795d7272c78cba60821af15031cad
Tim Lovorn <tflovorn@austin.utexas.edu>
2017-12-26 15:33:27 -0600

    include PETSc top-level header instead of petscksp

--- wrapper.h
+++ wrapper.h
@@ -1,1 +1,5 @@
-#include <petscksp.h>
+// `petsc.h` has the comment that:
+// 'This include file allows you to use ANY public PETSc function'.
+// Here we assume that this is correct, i.e. that only `petsc.h` needs to be included here
+// to access all PETSc functionality.
+#include <petsc.h>
